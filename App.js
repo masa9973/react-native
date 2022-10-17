4df6711cdcc19ec7d7790854b288eb1ec2dd6979
masa9973 <masamichi.ishikawa19990730@gmail.com>
2022-10-18 04:44:39 +0900

    update app.js

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import { StatusBar } from "expo-status-bar";
+import { useEffect, useState, useRef } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+    console.log("appfunc");
+
+    const ishikawa = "いしかわ！";
+
+    const [iskw, setIskw] = useState(ishikawa);
+
+    const [x, setX] = useState(0);
+    const refX = useRef(x);
+    useEffect(() => {
+        refX.current = x;
+    }, [x]);
+    useEffect(() => {
+        setInterval(() => {
+            setIskw(`${ishikawa.repeat(refX.current)}`);
+            console.log(refX.current);
+            setX(refX.current++);
+        }, 500);
+    }, []);
+
     return (
         <View style={styles.container}>
-            <Text>テスト！</Text>
+            <Text>{iskw}</Text>
             <StatusBar style='auto' />
         </View>
     );
